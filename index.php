2928d7d305655fad95d27d53a003b4bc66d66a20
vvediteima <54665155+vvediteima@users.noreply.github.com>
2020-03-06 17:18:22 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+if ($_REQUEST['act']=='mes') {
+$user=$_REQUEST['user'];
+    $mes=$_REQUEST['mes'];
+}
 $token = '141552c74b88c81395f0fe00a993b77a847d4359f0f64b141965208ce14c52d06f84a4dc583ac7c2565fe';
 $request_params = array(
 'message' => "admin hello",
