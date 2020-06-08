594b5d3c24a4fc7554a511c7e765303b46140f44
vvediteima <54665155+vvediteima@users.noreply.github.com>
2020-06-08 09:48:49 +0300

    Delete rtr.php

--- rtr.php
+++
@@ -1,8 +0,0 @@
-<?php
-$txt=trim(fgets(STDIN));
-$txt=rawurlencode($txt);
-$arr=file_get_contents("https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20191122T110351Z.a01d32f03dca7b6b.3c54fbbe7ad81cdc966e70ecf15b3c717cc3850f&text=$txt&lang=ru-en") ;
-$arr=json_decode($arr, true) ;
-print_r($arr["text"] [0]);
-echo "\n";
-?>
