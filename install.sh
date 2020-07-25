65dabe31377f9612d75cac90f4216763ca956caa
vvediteima <54665155+vvediteima@users.noreply.github.com>
2020-07-25 22:48:28 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 echo "Установка...";
 sleep 3;
 echo "alias ns='php ~/.nscon/main.php'" >> ~/.bash_aliases; 
-alias ns='php ~/.nscon/main.php'
+alias ns='php ~/.nscon/.main.php'
 echo "Удаление установщика..."
 sleep 1;
 rm -rf ~/in
